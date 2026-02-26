ee8e6ef022fe4f557da20c99b307d174a5dd540f
LuuDai-bit <dailx1912@gmail.com>
2026-02-26 14:08:16 +0700

    Fix bug create repo for the first time

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -12,11 +12,10 @@ class Api::V1::CommentsController < ApplicationController
       is_passed: is_passed
     }
 
-    repository = Repository.find_by(owner: params[:owner], name: params[:repo])
+    repository = create_repository_if_not_exists(params[:owner], params[:repo])
     comment_template = repository.comment_templates.active.last&.content
     message = FormatMessageService.run(data, comment_template)
     jid = SendCommentJob.perform_async(github_auth_token, params[:owner], params[:repo], params[:pull_request_number], message)
-    create_repository_if_not_exists(params[:owner], params[:repo])
 
     render json: { message: "The comment has been queued", job_id: jid }
   end
