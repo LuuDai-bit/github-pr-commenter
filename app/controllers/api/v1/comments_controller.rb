78871f9044895a30e54474cec3a75bc378473bee
LuuDai-bit <dailx1912@gmail.com>
2026-03-11 09:19:43 +0700

    Service for rest api

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CommentsController < ApplicationController
   def create
-    jid = PushCommentService.run(params)
+    jid = ::V1::PushCommentService.run(params)
 
     render json: { message: "The comment has been queued", job_id: jid }
   end
